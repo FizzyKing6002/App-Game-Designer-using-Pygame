d16f8608313320ea54ce8b98d61de0fba9997cd3
FizzyKing6002 <87538561+FizzyKing6002@users.noreply.github.com>
2023-10-26 14:55:15 +0100

    Empty Functions Added
    Empty functions for all of the classes have been created according to their needs.

--- ImportCode/objects.py
+++ ImportCode/objects.py
@@ -13,22 +13,42 @@ def Object(*args):
             self.opacity = 1
             self.script_directory = args[0]
 
+        def __call__(self, window, time,
+                     con_pos, con_size, con_rot, con_opa,
+                     mouse_pos, mouse_state, key_state):
+            pass
+
+        def animate(self, time):
+            pass
+
+        def calc_attr(self, con_pos, con_size, con_rot, con_opa):
+            pass
+
     return Object(args)
 
 class Container:
-    pass
+    def __init__(self):
+        self.objects = []
+
+    def call_objects(self, window, time,
+                     mouse_pos, mouse_state, key_state):
+        pass
 
 class Image:
-    pass
+    def draw_self(self):
+        pass
 
 class Button:
-    pass
+    def call_clicked(self, mouse_pos, mouse_state):
+        pass
 
 class Hover_Activated:
-    pass
+    def call_hovered(self, mouse_pos):
+        pass
 
 class Key_Activated:
-    pass
+    def call_activated(self, key_state):
+        pass
 
 class None1:
     pass
