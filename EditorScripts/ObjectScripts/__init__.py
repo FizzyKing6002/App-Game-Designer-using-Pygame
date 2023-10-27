cf960cd6aa5e8fac2c941c0068e5448d0ca74c92
FizzyKing6002 <87538561+FizzyKing6002@users.noreply.github.com>
2023-10-27 15:48:32 +0100

    Bug Fix when Importing Modules
    Made use of the __import__ syntax to change the import to being relative.
    Bugged, attributes of these files not found.

--- EditorScripts/ObjectScripts/__init__.py
+++ EditorScripts/ObjectScripts/__init__.py
@@ -3,5 +3,5 @@ import os
 for file in os.listdir(os.path.dirname(__file__)):
     if file == "__init__.py" or file[-3:] != ".py":
         continue
-    __import__(file[:-3], locals(), globals())
+    __import__(file[:-3], locals(), globals(), [], 1)
 del file
