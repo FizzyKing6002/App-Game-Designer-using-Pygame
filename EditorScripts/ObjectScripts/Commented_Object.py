a1f21ba5e1239dbe17424687c564b5f6f9178bb7
FizzyKing6002 <87538561+FizzyKing6002@users.noreply.github.com>
2023-11-15 09:56:32 +0000

    Added New Comments for Object Code
    Added comments detailing how to create animations for objects.

--- EditorScripts/ObjectScripts/Commented_Object.py
+++ EditorScripts/ObjectScripts/Commented_Object.py
@@ -32,6 +32,7 @@ class Commented_Object:
         # For rotated containers and containers that have a scroll bar, this becomes False
         self.objects_visible_outside_container = True
         # Image directory for this object (path from main.py) - IMAGE ONLY
+        # If image does not exist, defaults to black rectangle
         self.img_dir = ""
         # Dictionary of keys that activate object ("[key_name]" : True/False) - KEY_ACTIVATED ONLY
         self.activation_keys = {}
@@ -61,6 +62,9 @@ class Commented_Object:
         self.text_bold = False
         self.text_italic = False
 
+        # Additional attributes:
+
+
     # Called every frame, passes the object of globalScripts.py class
     def frame_update(self, global_scripts):
         pass
@@ -84,3 +88,27 @@ class Commented_Object:
     # Called for each key in self.activation_keys that was pressed this frame, passes key name
     def key_input(self, key):
         pass
+
+    # Additional methods:
+
+
+"""
+To create an animation:
+Call self.create_animation(x, y, z{, a})
+    x -> float -> final value of the animation (initial value is always zero)
+    y -> int -> total time for the animation to take place over in milliseconds
+    z -> string -> type of animation ->
+        include pos (position), size, rot (rotation) or opa (opacity)
+        include x, y or neither for position and size animations
+            if neither, its assumed the animation is in both x and y directions
+            if neither, a list can be used for x and a
+        include % if the animation controls the percentage modifiers
+    a -> this parameter can be ignored (therefore is shown in curly braces) ->
+        string -> easing function for animation -> use "x" as the subject of the function
+        if none given, defaults to linear easing function
+e.g. self.create_animation(500, 5000, "posx") ->
+    moves object 500 pixels to the right over 5 seconds
+e.g. self.create_animation([0.5, 0.1], 2000, "size%", ["x", "x**3"]) ->
+    resizes the object in both x and y by percentages of their container over 2 seconds
+Remember to only create the animation once as animations stack with each other
+"""
