67a17ccbeedc1604dcff89590822b1846f7b3b41
FizzyKing6002 <87538561+FizzyKing6002@users.noreply.github.com>
2023-12-07 11:01:48 +0000

    Edit File upon Creation
    Files are now edited when they are created so that the colour of the new object persists along with the size being accordingly scaled.
    Position needs to be adjusted.

--- EditorAssets/EditorScripts/globalScripts.py
+++ EditorAssets/EditorScripts/globalScripts.py
@@ -52,20 +52,58 @@ class globalScripts:
         if self.create_new:
             self.create_new = False
 
-            project_name = os.listdir("_CurrentProject")[0]
-            shutil.copy("EditorAssets/CodeStructs/Commented_Object.py",
-                        f"_CurrentProject/{project_name}/Assets/Scripts/ObjectScripts")
+            self.create_object_file()
 
-            while True:
-                try:
-                    os.rename(f"_CurrentProject/{project_name}/Assets/Scripts/ObjectScripts/\
-Commented_Object.py",
-                            f"_CurrentProject/{project_name}/Assets/Scripts/ObjectScripts/\
-Commented_Object{self.generated_obj_num}.py")
+    def create_object_file(self):
+        project_name = os.listdir("_CurrentProject")[0]
+        shutil.copy("EditorAssets/CodeStructs/Commented_Object.py",
+                    f"_CurrentProject/{project_name}/Assets/Scripts/ObjectScripts")
 
-                except FileExistsError:
-                    self.generated_obj_num += 1
-                    continue
+        while True:
+            try:
+                path = f"_CurrentProject/{project_name}/Assets/Scripts/ObjectScripts/\
+Commented_Object{self.generated_obj_num}.py"
+                os.rename(f"_CurrentProject/{project_name}/Assets/Scripts/ObjectScripts/\
+Commented_Object.py", path)
+
+            except FileExistsError:
+                self.generated_obj_num += 1
+                continue
+            break
+
+        self.generated_obj_num += 1
+
+#        self.change_file_str(path, "[[0, 0.5], [0, 0.5]]",
+ #                               f"[[0, {self.generator_pos[0]}], [0, {self.generator_pos[1]}]]",
+  #                              "self.position_modifiers")
+        self.change_file_str(path, "[[0, 1], [0, 1]]",
+                                "[[0, 0.15], [0, 0.15]]",
+                                "self.size_modifiers")
+        self.change_file_str(path, "(0, 0, 0)", f"{self.generator_colour}", "self.object_colour")
+
+    def change_file_str(self, path, target, new_val, *required_strings):
+        with open(path, "r") as file:
+            file_data = file.read()
+        
+        split_data = file_data.split("\n")
+        for i, line in enumerate(split_data):
+            for string in required_strings:
+                if string not in line:
+                    break
+            else:
+                new_data = ""
+                split_data[i] = line.replace(target, new_val)
+                for new_line in split_data:
+                    new_data += new_line + "\n"
+                else:
+                    if len(new_data) >= 2:
+                        new_data = new_data[:-1]
+
+                with open(f"{path[:-3]}%__cache__%.py", "w") as file:
+                    file.write(new_data)
+                
+                with open(path, "w") as file:
+                    file.write(new_data)
+
+                os.remove(f"{path[:-3]}%__cache__%.py")
                 break
-
-            self.generated_obj_num += 1
